3c70477fe7061c0546ca40352a18d0e78bc4b266
Yanina Bortol <yaninabortol@gmail.com>
2023-09-25 17:51:30 -0300

    Primer escritura en el repositorio

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.getElementById("boton").addEventListener('click', function() {
+    document.getElementById('sobreYani').style.display='none';
+})
+
+document.getElementById("boton2").addEventListener('click', function() {
+    document.getElementById('sobreYani').style.display='block';
+})
